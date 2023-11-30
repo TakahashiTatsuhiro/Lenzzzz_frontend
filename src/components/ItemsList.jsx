bb60fa5c37177c0ce9170b41d4ad38c01210546c
naoya <naoya0517.mc@gmail.com>
2023-11-30 14:12:34 +0900

    登録後のリダイレクトを実装

--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -3,63 +3,70 @@ import { useAuth } from '../contexts/AuthContext';
 import { useEffect, useState } from 'react';
 
 const ItemsList = () => {
-	const navigate = useNavigate();
-	const { userId } = useAuth();
-	const [items, setItems] = useState([]);
+  const navigate = useNavigate();
+  const { userId } = useAuth();
+  const [items, setItems] = useState([]);
 
-	useEffect(() => {
-		const getItems = async () => {
-			try {
-				// const url = 'https://lenzzzz-backend.onrender.com';
-				const url = 'http://localhost:3000';
-				const response = await fetch(url + `/items/${userId}`);
-				const data = await response.json();
-				if (response.ok) {
-					console.log('data', data);
-					setItems(data);
-				} else {
-					setItems([]);
-				}
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		getItems();
-	}, []);
+  useEffect(() => {
+    const getItems = async () => {
+      try {
+        // const url = 'https://lenzzzz-backend.onrender.com';
+        const url = 'http://localhost:3000';
+        const response = await fetch(url + `/items/${userId}`);
+        const data = await response.json();
+        if (response.ok) {
+          console.log('data', data);
+          setItems(data);
+        } else {
+          setItems([]);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getItems();
+  }, []);
 
-	const handleLogin = () => {
-		navigate('/login');
-	};
+  const handleLogin = () => {
+    navigate('/login');
+  };
 
-	const handleRegistration = () => {
-		navigate('/registrations');
-	};
+  const handleRegistration = () => {
+    navigate('/registrations');
+  };
 
-	return (
-		<div className='items__container'>
-			<div className='items__header'>
-				<button className='items__btn' onClick={handleLogin}>
-					戻る
-				</button>
-				<input className='items__serach' placeholder='商品名を入力してください'></input>
-			</div>
-			<div className='items__add'>
-				<button className='items__btn' onClick={handleRegistration}>
-					新しいアイテムを追加
-				</button>
-			</div>
-			<div className='items__list'>
-				{items.map((item, idx) => {
-					return (
-						<div className='item' key={idx}>
-							<img className='item__img' src={item.product_photo} alt={item.product_name} />
-							<p className='item__name'>{item.product_name}</p>
-						</div>
-					);
-				})}
-			</div>
-		</div>
-	);
+  return (
+    <div className="items__container">
+      <div className="items__header">
+        <button className="items__btn" onClick={handleLogin}>
+          ログアウト
+        </button>
+        <input
+          className="items__serach"
+          placeholder="商品名を入力してください"
+        ></input>
+      </div>
+      <div className="items__add">
+        <button className="items__btn" onClick={handleRegistration}>
+          新しいアイテムを追加
+        </button>
+      </div>
+      <div className="items__list">
+        {items.map((item, idx) => {
+          return (
+            <div className="item" key={idx}>
+              <img
+                className="item__img"
+                src={item.product_photo}
+                alt={item.product_name}
+              />
+              <p className="item__name">{item.product_name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ItemsList;
--- src/components/Registrations.jsx
+++ src/components/Registrations.jsx
@@ -4,160 +4,167 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
 const Registrations = () => {
-	const navigate = useNavigate();
-	const { userId } = useAuth();
+  const navigate = useNavigate();
+  const handleItems = () => {
+    navigate('/items');
+  };
 
-	const [productName, setProductName] = useState(false);
-	const [purchaseDate, setPurchaseDate] = useState(false);
-	const [warrantyNumber, setWarrantyNumber] = useState(false);
-	const [warrantyUnit, setWarrantyUnit] = useState(false);
-	const [retailer, setRetailer] = useState(false);
-	const [productPhoto, setProductphoto] = useState('');
-	const [warrantyPhoto, setWarrantyPhoto] = useState('');
+  const { userId } = useAuth();
 
-	const refProductPhoto = React.useRef();
-	const refWarrantyPhoto = React.useRef();
-	const handleClickProductPhoto = () => refProductPhoto.current.click();
-	const handleButtonWarrantyPhoto = () => refWarrantyPhoto.current.click();
-	const handleSetPhoto = (e, setfunc) => {
-		const file = e.target.files[0];
-		if (file) {
-			const reader = new FileReader();
-			reader.onload = (e) => {
-				setfunc(e.target.result);
-			};
-			reader.readAsDataURL(file);
-		}
-	};
+  const [productName, setProductName] = useState(false);
+  const [purchaseDate, setPurchaseDate] = useState(false);
+  const [warrantyNumber, setWarrantyNumber] = useState(false);
+  const [warrantyUnit, setWarrantyUnit] = useState(false);
+  const [retailer, setRetailer] = useState(false);
+  const [productPhoto, setProductphoto] = useState('');
+  const [warrantyPhoto, setWarrantyPhoto] = useState('');
 
-	const handleItems = () => {
-		navigate('/items');
-	};
+  const refProductPhoto = React.useRef();
+  const refWarrantyPhoto = React.useRef();
+  const handleClickProductPhoto = () => refProductPhoto.current.click();
+  const handleButtonWarrantyPhoto = () => refWarrantyPhoto.current.click();
+  const handleSetPhoto = (e, setfunc) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setfunc(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
-	const handleSubmit = async () => {
-		//デバッグ用
-		console.log('productName', productName);
-		console.log('購入日', purchaseDate);
-		console.log('保証', warrantyNumber);
-		console.log('保証単位', warrantyUnit);
-		console.log('購入店', retailer);
-		console.log('商品画像', productPhoto);
-		console.log('保証書画像', warrantyPhoto);
+  const handleSubmit = async () => {
+    //デバッグ用
+    console.log('productName', productName);
+    console.log('購入日', purchaseDate);
+    console.log('保証', warrantyNumber);
+    console.log('保証単位', warrantyUnit);
+    console.log('購入店', retailer);
+    console.log('商品画像', productPhoto);
+    console.log('保証書画像', warrantyPhoto);
 
-		//未入力チェック
-		if (!productName || !purchaseDate || !warrantyNumber || !warrantyUnit || !retailer) {
-			console.log('未入力だよ');
-		}
+    //未入力チェック
+    if (
+      !productName ||
+      !purchaseDate ||
+      !warrantyNumber ||
+      !warrantyUnit ||
+      !retailer
+    ) {
+      console.log('未入力だよ');
+    }
 
-		//送信
-		try {
-			// const url = 'https://lenzzzz-backend.onrender.com';
-			const url = "http://localhost:3000";
+    //送信
+    try {
+      // const url = 'https://lenzzzz-backend.onrender.com';
+      const url = 'http://localhost:3000';
 
-			const response = await fetch(url + '/registrations', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					user_id: userId,
-					product_name: productName,
-					purchase_date: purchaseDate,
-					warranty_number: warrantyNumber,
-					warranty_unit: warrantyUnit,
-					retailer: retailer,
-					product_photo: productPhoto,
-					warranty_photo: warrantyPhoto,
-				}),
-			});
+      const response = await fetch(url + '/registrations', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user_id: userId,
+          product_name: productName,
+          purchase_date: purchaseDate,
+          warranty_number: warrantyNumber,
+          warranty_unit: warrantyUnit,
+          retailer: retailer,
+          product_photo: productPhoto,
+          warranty_photo: warrantyPhoto,
+        }),
+      });
 
-			// const data = await response.json();
-			if (response.ok) {
-				window.alert('登録しました！');
-			} else {
-				window.alert('登録に失敗しました！');
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+      // const data = await response.json();
+      if (response.ok) {
+        window.alert('登録しました！');
+        handleItems();
+      } else {
+        window.alert('登録に失敗しました！');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	return (
-		<div className='registration__container'>
-			<div className='items__header'>
-				<button className='items__btn' onClick={handleItems}>
-					戻る
-				</button>
-			</div>
-			<div className='registration__box'>
-				<p className='login__input-title'>商品名</p>
-				<input
-					className='login__input'
-					type='text'
-					placeholder='例）冷蔵庫'
-					onChange={(e) => setProductName(e.target.value)}
-				/>
-				<p className='login__input-title'>購入日</p>
-				<input
-					className='login__input'
-					type='date'
-					placeholder='yyyy/mm/dd'
-					onChange={(e) => setPurchaseDate(e.target.value)}
-				/>
-				<p className='login__input-title'>保証期間</p>
-				<input
-					className='login__input'
-					type='number'
-					placeholder='例）３'
-					onChange={(e) => setWarrantyNumber(e.target.value)}
-				/>
-				<select
-					name='unit'
-					className='login__input'
-					onChange={(e) => setWarrantyUnit(e.target.value)}
-				>
-					<option>年</option>
-					<option>月</option>
-				</select>
-				<p className='login__input-title'>購入先</p>
-				<input
-					className='login__input'
-					type='text'
-					placeholder='例）エディオン本店'
-					onChange={(e) => setRetailer(e.target.value)}
-				/>
+  return (
+    <div className="registration__container">
+      <div className="items__header">
+        <button className="items__btn" onClick={handleItems}>
+          戻る
+        </button>
+      </div>
+      <div className="registration__box">
+        <p className="login__input-title">商品名</p>
+        <input
+          className="login__input"
+          type="text"
+          placeholder="例）冷蔵庫"
+          onChange={(e) => setProductName(e.target.value)}
+        />
+        <p className="login__input-title">購入日</p>
+        <input
+          className="login__input"
+          type="date"
+          placeholder="yyyy/mm/dd"
+          onChange={(e) => setPurchaseDate(e.target.value)}
+        />
+        <p className="login__input-title">保証期間</p>
+        <input
+          className="login__input"
+          type="number"
+          placeholder="例）３"
+          onChange={(e) => setWarrantyNumber(e.target.value)}
+        />
+        <select
+          name="unit"
+          className="login__input"
+          onChange={(e) => setWarrantyUnit(e.target.value)}
+        >
+          <option>年</option>
+          <option>月</option>
+        </select>
+        <p className="login__input-title">購入先</p>
+        <input
+          className="login__input"
+          type="text"
+          placeholder="例）エディオン本店"
+          onChange={(e) => setRetailer(e.target.value)}
+        />
 
-				<p className='login__input-title'>商品写真</p>
-				<button className='button' onClick={handleClickProductPhoto}>
-					商品画像を選択
-				</button>
-				<input
-					type='file'
-					style={{ display: 'none' }}
-					ref={refProductPhoto}
-					onChange={(e) => handleSetPhoto(e, setProductphoto)}
-				/>
-				{productPhoto && <img src={productPhoto} alt='商品の写真'></img>}
+        <p className="login__input-title">商品写真</p>
+        <button className="button" onClick={handleClickProductPhoto}>
+          商品画像を選択
+        </button>
+        <input
+          type="file"
+          style={{ display: 'none' }}
+          ref={refProductPhoto}
+          onChange={(e) => handleSetPhoto(e, setProductphoto)}
+        />
+        {productPhoto && <img src={productPhoto} alt="商品の写真"></img>}
 
-				<p className='login__input-title'>保証書写真</p>
-				<button className='button' onClick={handleButtonWarrantyPhoto}>
-					保証書画像を選択
-				</button>
-				<input
-					type='file'
-					style={{ display: 'none' }}
-					ref={refWarrantyPhoto}
-					onChange={(e) => handleSetPhoto(e, setWarrantyPhoto)}
-				/>
-				{warrantyPhoto && <img src={warrantyPhoto} alt='保証書の写真'></img>}
-			</div>
-			<div className='registration__footer'>
-				<button className='items__btn' onClick={handleSubmit}>
-					上記の内容で保存する
-				</button>
-			</div>
-		</div>
-	);
+        <p className="login__input-title">保証書写真</p>
+        <button className="button" onClick={handleButtonWarrantyPhoto}>
+          保証書画像を選択
+        </button>
+        <input
+          type="file"
+          style={{ display: 'none' }}
+          ref={refWarrantyPhoto}
+          onChange={(e) => handleSetPhoto(e, setWarrantyPhoto)}
+        />
+        {warrantyPhoto && <img src={warrantyPhoto} alt="保証書の写真"></img>}
+      </div>
+      <div className="registration__footer">
+        <button className="items__btn" onClick={handleSubmit}>
+          上記の内容で保存する
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Registrations;
