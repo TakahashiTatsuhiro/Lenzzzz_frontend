0e0ba7bed77eb239142bfa02702f2b1a0f0f3b65
Yoshi2885 <yoshitaka_tanaka_aa@mail.toyota.co.jp>
2023-12-01 19:00:44 +0900

    credentials 追加

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 const LoginForm = () => {
-  const [user_name, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [user_name, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const { login, setUserId, setUserName } = useAuth();
 
   const handleSubmit = async () => {
-    console.log('送信時', user_name, password);
+    console.log("送信時", user_name, password);
     console.log(
-      'json',
+      "json",
       JSON.stringify({ user_name: user_name, password: password })
     );
     try {
@@ -22,12 +22,13 @@ const LoginForm = () => {
       const url =
         import.meta.env.VITE_DEVELOPMENT_BACKEND_URL ||
         import.meta.env.VITE_PRODUCTION_BACKEND_URL;
-      console.log('最終的なURLは?', url);
+      console.log("最終的なURLは?", url);
 
-      const response = await fetch(url + '/login', {
-        method: 'POST',
+      const response = await fetch(url + "/login", {
+        method: "POST",
+        credentials: "include",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify([{ user_name: user_name, password: password }]),
       });
@@ -36,7 +37,7 @@ const LoginForm = () => {
         setUserId(data[0].id);
         setUserName(user_name);
         login();
-        navigate('/items'); // ここでItemsListコンポーネントへ遷移
+        navigate("/items"); // ここでItemsListコンポーネントへ遷移
       }
     } catch (error) {
       console.log(error);
@@ -44,7 +45,7 @@ const LoginForm = () => {
   };
 
   const handleRegistUser = () => {
-    navigate('/users/new');
+    navigate("/users/new");
   };
 
   return (
